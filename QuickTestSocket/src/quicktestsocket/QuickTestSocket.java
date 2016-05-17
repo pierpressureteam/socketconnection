ac71aec208e0c836b53bab672e06db541264e84b
RoyvandenHeuvel <0878963@hr.nl>
2016-05-17 13:33:29 +0200

    polished

--- QuickTestSocket/src/quicktestsocket/QuickTestSocket.java
+++ QuickTestSocket/src/quicktestsocket/QuickTestSocket.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package quicktestsocket;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import ships.Ship;
-
 
 /**
  *
@@ -19,12 +14,11 @@ public class QuickTestSocket {
 
     static final String HOST = "127.0.0.1";
     static final int PORT = 4444;
-    
+
     public static void main(String[] args) {
         testSocket();
     }
 
-    
     public static void testSocket() {
         try {
             // Create the socket
@@ -33,29 +27,29 @@ public class QuickTestSocket {
             ObjectOutputStream outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
             ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
 
-            /* Create The Message Object to send */
+            /* Create object to send */
             Ship msg = new Ship(10000, "Groot");
+            
+            System.out.println("Object as it was sent: \n");
+            System.out.println(msg.getMMSI());
+            System.out.println(msg.getType());
 
-            /* Send the Message Object to the server */
+            /* Send the object to the server */
             outToServer.writeObject(msg);
 
-            /* Retrieve the Message Object from server */
-            Ship msgFrmServer = null;
-            msgFrmServer = (Ship) inFromServer.readObject();
-
-            /* Print out the recived Message */
-            System.out.println("MMSI: " + msgFrmServer.getMMSI());
-            System.out.println("Type: " + msgFrmServer.getType());
-
+            /* Retrieve the object from server */
+            Ship msgFrmServer = (Ship) inFromServer.readObject();
+            
+            System.out.println("Object as it was received again: \n");
+            System.out.println(msgFrmServer.getMMSI());
+            System.out.println(msgFrmServer.getType());
+            
             outToServer.flush();
             outToServer.close();
             inFromServer.close();
             clientSocket.close();
 
-        } catch (Exception e) {
-            System.err.println("Client Error: " + e.getMessage());
-            System.err.println("Localized: " + e.getLocalizedMessage());
-            System.err.println("Stack Trace: " + e.getStackTrace());
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
--- SocketConnectionServer/src/socketconnectionserver/SocketConnectionServer.java
+++ SocketConnectionServer/src/socketconnectionserver/SocketConnectionServer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package socketconnectionserver;
 
 import java.io.IOException;
@@ -21,14 +16,11 @@ public class SocketConnectionServer {
     static final int PORT = 4444;
 
     public static void main(String[] args) {
-
         startServer();
-
     }
 
     public static void startServer() {
         try {
-
             ServerSocket welcomeSocket = new ServerSocket(PORT);
             System.out.println("Socket created.");
 
@@ -41,23 +33,26 @@ public class SocketConnectionServer {
                 ObjectOutputStream outToClient = new ObjectOutputStream(clientSocket.getOutputStream());
                 ObjectInputStream inFromClient = new ObjectInputStream(clientSocket.getInputStream());
 
+                /* Read the object the client has sent from the inputstream */
                 Ship inMsg = (Ship) inFromClient.readObject();
+                System.out.println("Object as it was received: \n");
                 System.out.println(inMsg.getMMSI());
                 System.out.println(inMsg.getType());
 
+                /* Edits the object a little bit */
                 inMsg.setMMSI(inMsg.getMMSI() + 5000);
                 inMsg.setType("Groter");
 
+                System.out.println("Object as it was sent: \n");
+                System.out.println(inMsg.getMMSI());
+                System.out.println(inMsg.getType());
 
-                /* Send the modified Message object back */
+                /* Send the modified object back */
                 outToClient.writeObject(inMsg);
 
             }
 
         } catch (IOException | ClassNotFoundException e) {
-            System.err.println("Server Error: " + e.getMessage());
-            System.err.println("Localized: " + e.getLocalizedMessage());
-            System.err.println("To String: " + e.toString());
             e.printStackTrace();
         }
     }
