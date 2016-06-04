aab05e5a0380bcea9f7e43464a7f1630fe0ee4ba
RoyvandenHeuvel <0878963@hr.nl>
2016-06-04 17:33:14 +0200

    oscar's server in use now

--- QuickTestSocket/src/socketclient/SocketClient.java
+++ QuickTestSocket/src/socketclient/SocketClient.java
@@ -14,11 +14,12 @@ public class SocketClient
 {
 
     private final static String HOST = "145.24.222.149";
+    private final static String OSCARHOST = "77.163.77.9";
     private final static int PORT = 32007;
 
     public Object communicateWithSocket(SocketObjectWrapper sow) throws IOException, ClassNotFoundException
     {
-        Socket clientSocket = new Socket(HOST, PORT);
+        Socket clientSocket = new Socket(OSCARHOST, PORT);
 
         ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
         ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
